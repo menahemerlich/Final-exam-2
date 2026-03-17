9f9bafa1f8b2b8e28e807fe523602f89f59ef381
menahem_erlich <erlichmenahem@gmail.com>
2026-03-17 10:12:48 +0200

    add a new user endpoint

--- server/routs/registerRoute.js
+++ server/routs/registerRoute.js
@@ -1,19 +1,21 @@
 import express from 'express'
+import { userTypes } from '../utils/typeChecking.js'
+import { db } from '../db/connectDB.js'
 
 export const registerRoute = express.Router()
 
-// registerRoute.post('/create', async (req, res) => {
-//     if (req.body && typeChecking(req.body)) {
-//             const { name, rocketType, latitude, longitude, city } = req.body
-//             const newLauncher = await db.collection('launchers').insertOne({
-//                 name,
-//                 rocketType,
-//                 latitude,
-//                 longitude,
-//                 city
-//             })
-//             return res.status(200).json({ id: newLauncher.insertedId })
+registerRoute.post('/create', async (req, res) => {
+    if (req.body && userTypes(req.body)) {
+            const { userName, password, email, user_type, last_login } = req.body
+            const newUser = await db.collection('users').insertOne({
+                userName,
+                password,
+                email,
+                user_type,
+                last_login
+            })
+            return res.status(200).json({ id: newUser.insertedId })
         
-//     }
-//     return res.status(400).json({ message: "invalid files" })
-// })
+    }
+    return res.status(400).json({ message: "invalid files" })
+})
